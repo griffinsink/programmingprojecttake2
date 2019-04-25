3bc3c6c08fca6276e59fc08934585503390e83b2
Jack Markmiller <jackmarkmiller@ou.edu>
2019-04-25 13:24:20 -0500

    Thursday

--- ConnectFour/ConnectFour/MainWindow.xaml.cs
+++ ConnectFour/ConnectFour/MainWindow.xaml.cs
@@ -20,9 +20,93 @@ namespace ConnectFour
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int chipSize = 80;
+
+        private boardgame board;
+        private bool inputLock;
+        
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            inputLock = true;
+            board = new boardgame(6, 7);
+
+
+        }
+        
+        private void Button_Click(int column)
+        {
+            if(inputLock == false)
+            {
+                bool success = board.Insert(currentSide, column);
+                if (success)
+                {
+                    currentColumn = column;
+                   
+                }
+                    
+            }
+        }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(0);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(1);
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(2);
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(3);
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(4);
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(5);
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click(6);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void ButtonRestart_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
         }
     }
 }
--- ConnectFour/ConnectFour/boardgame.cs
+++ ConnectFour/ConnectFour/boardgame.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ConnectFour
 {
-    class boardgame
+
+    public enum Side { None, Red, Blue };
+
+    public class boardgame
     {
+        public Side[,] Gameboard { get; private set; }
+
+        public boardgame(int rows, int columns)
+        {
+            Gameboard = new Side[rows, columns];
+            for (int row = 0; row < this.Gameboard.GetLength(0); row++)
+                for (int col = 0; col < this.Gameboard.GetLength(1); col++)
+                    this.Gameboard[row, col] = Side.None;
+        }
+
+
     }
 }
